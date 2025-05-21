9f7bbd3b341753caf5074503fc3ea5ae20ed01be
Edyn <triminh.nguyen2098@gmail.com>
2025-05-21 21:10:46 +0700

    [Day_5] handle error

--- internal/controllers/user.controller.go
+++ internal/controllers/user.controller.go
@@ -1,9 +1,8 @@
 package controllers
 
 import (
-	"net/http"
-
 	"github.com/edynt/go-ecommerce-api/internal/services"
+	"github.com/edynt/go-ecommerce-api/response"
 	"github.com/gin-gonic/gin"
 )
 
@@ -21,13 +20,5 @@ func NewUserController() *UserController { // tra ve con tro
 // us: user service
 
 func (uc *UserController) GetUserByID(c *gin.Context) {
-	name := c.Param("name")
-	uid := c.DefaultQuery("uid", "0000000")
-
-	c.JSON(http.StatusOK, gin.H{
-		"message": uc.userService.GetInfoUser(),
-		"name":    name,
-		"uid":     uid,
-		"users":   []string{"cr7", "m10", "tringuyen"},
-	})
+	response.SuccessResponse(c, 2001, uc.userService.GetInfoUser())
 }
--- response/httpStatusCode.go
+++ response/httpStatusCode.go
@@ -1,1 +1,12 @@
 package response
+
+const (
+	ErrCodeSuccess      = 2001 // Success
+	ErrCodeParamInvalid = 2003 // Email is invalid
+)
+
+// message
+var msg = map[int]string{
+	ErrCodeSuccess:      "Success",
+	ErrCodeParamInvalid: "Email is invalid",
+}
